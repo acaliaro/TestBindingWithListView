a521f48959c48847f9f45a9248408ca8dfcb3093
Alessandro Caliaro <alecaliaro@inwind.it>
2017-01-13 09:31:43 +0100

    To solve the “Command” problem (Use Command present in Page’s ViewModel also in ListView Template) I have moved the Template definition inside the page and remove MyTemplate.cs

--- TestBinding/MyPage.cs
+++ TestBinding/MyPage.cs
@@ -12,12 +12,44 @@ namespace TestBinding
 			StackLayout sl = new StackLayout();
 			sl.Padding = new Thickness(20, 20, 20, 20);
 
-			Label l = new Label() { Text = "You can TAP on Description, Trash or on QTY... there are two different answers because Description raise a SelectedItem, Trash and Qty raise a TapGestureRecognizer. If you delete all rows, a Label appears. ATTENTION. NOW TRASH DOES NOT WORKS!!!" };
+			Label l = new Label() { Text = "You can TAP on Description, Trash or on QTY... there are two different answers because Description raise a SelectedItem, Trash and Qty raise a TapGestureRecognizer. If you delete all rows, a Label appears." };
 			Label labelEmpty = new Label { Text = "THE LIST IS EMPTY", VerticalOptions = LayoutOptions.CenterAndExpand, HorizontalOptions = LayoutOptions.CenterAndExpand };
 			labelEmpty.SetBinding(Label.IsVisibleProperty, "IsLabelEmptyVisible");
 
+			// Moved here the Template so I can use Commands in ViewModel also in ViewCell with this binding
+			//tgrQty.SetBinding(TapGestureRecognizer.CommandProperty, new Binding("BindingContext.QtyCommand", source: this));
+			//tgrQty.SetBinding(TapGestureRecognizer.CommandParameterProperty, ".");
+
 			ListView lv = new ListView { HasUnevenRows = true };
-			lv.ItemTemplate = new DataTemplate(typeof(MyTemplate));
+			lv.ItemTemplate = new DataTemplate(() =>
+			{
+				StackLayout slView = new StackLayout();
+
+				Label lDesc = new Label();
+				lDesc.SetBinding(Label.TextProperty, "Description", stringFormat: "DESCRIPTION: {0}");
+
+				// LABEL QTY
+				TapGestureRecognizer tgrQty = new TapGestureRecognizer();
+				tgrQty.SetBinding(TapGestureRecognizer.CommandProperty, new Binding("BindingContext.QtyCommand", source: this));
+				tgrQty.SetBinding(TapGestureRecognizer.CommandParameterProperty, ".");
+
+				Label lQty = new Label();
+				lQty.GestureRecognizers.Add(tgrQty);
+				lQty.SetBinding(Label.TextProperty, "Qty", stringFormat: "QTY: {0}");
+
+				// LABEL TRASH
+				TapGestureRecognizer tgrTrash = new TapGestureRecognizer();
+				tgrTrash.SetBinding(TapGestureRecognizer.CommandProperty, new Binding("BindingContext.TrashCommand", source: this));
+				tgrTrash.SetBinding(TapGestureRecognizer.CommandParameterProperty, ".");
+
+				Label lTrash = new Label { Text = "Trash" };
+				lTrash.GestureRecognizers.Add(tgrTrash);
+
+				slView.Children.Add(lDesc);
+				slView.Children.Add(lQty);
+				slView.Children.Add(lTrash);
+				return new ViewCell { View = slView};
+			});
 			lv.SetBinding(ListView.ItemsSourceProperty, "List");
 			lv.SetBinding(ListView.IsVisibleProperty, "IsListViewVisible");
 			lv.SetBinding(ListView.SelectedItemProperty, "SelectedItem");
