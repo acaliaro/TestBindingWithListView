051f1f87dc27bffe828d3237a06b39d9350f72cc
Alessandro Caliaro <alecaliaro@inwind.it>
2017-03-27 12:39:48 +0200

    Added ContextAction to ViewCell

--- TestBinding/MyPage.cs
+++ TestBinding/MyPage.cs
@@ -12,14 +12,11 @@ namespace TestBinding
 			StackLayout sl = new StackLayout();
 			sl.Padding = new Thickness(20, 20, 20, 20);
 
-			Label l = new Label() { Text = "You can TAP on Description, Trash or on QTY... there are two different answers because Description raise a SelectedItem, Trash and Qty raise a TapGestureRecognizer. If you delete all rows, a Label appears." };
+			Label l = new Label() { Text = "You can TAP on Description, Trash or on QTY... there are two different answers because Description raise a SelectedItem, Trash and Qty raise a TapGestureRecognizer. If you delete all rows, a Label appears. If you longpress a row, a ContextAction is executed" };
 			Label labelEmpty = new Label { Text = "THE LIST IS EMPTY", VerticalOptions = LayoutOptions.CenterAndExpand, HorizontalOptions = LayoutOptions.CenterAndExpand };
 			labelEmpty.SetBinding(Label.IsVisibleProperty, "IsLabelEmptyVisible");
 
 			// Moved here the Template so I can use Commands in ViewModel also in ViewCell with this binding
-			//tgrQty.SetBinding(TapGestureRecognizer.CommandProperty, new Binding("BindingContext.QtyCommand", source: this));
-			//tgrQty.SetBinding(TapGestureRecognizer.CommandParameterProperty, ".");
-
 			ListView lv = new ListView { HasUnevenRows = true };
 			lv.ItemTemplate = new DataTemplate(() =>
 			{
@@ -45,10 +42,17 @@ namespace TestBinding
 				Label lTrash = new Label { Text = "Trash" };
 				lTrash.GestureRecognizers.Add(tgrTrash);
 
+				var deleteAction = new MenuItem { Text = "Delete", IsDestructive = true }; // red background
+				deleteAction.SetBinding(MenuItem.CommandParameterProperty, new Binding("BindingContext.TrashCommand", source: this));
+
 				slView.Children.Add(lDesc);
 				slView.Children.Add(lQty);
 				slView.Children.Add(lTrash);
-				return new ViewCell { View = slView};
+
+				ViewCell vc = new ViewCell() {View = slView };
+				vc.ContextActions.Add(deleteAction);
+
+				return vc;
 			});
 			lv.SetBinding(ListView.ItemsSourceProperty, "List");
 			lv.SetBinding(ListView.IsVisibleProperty, "IsListViewVisible");
